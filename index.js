c7cac77f048bfc88d9b158dfe4bf5b6413087be1
Kinzamalik <kiinza.malik@gmail.com>
2021-06-13 11:37:35 +0500

    adding css

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // 26101fa61746bcab8ec309c6f0c49f2c
 
-api.openweathermap.org/data/2.5/weather?q=Pune&appid=26101fa61746bcab8ec309c6f0c49f2c
+// api.openweathermap.org/data/2.5/weather?q=Pune&appid=26101fa61746bcab8ec309c6f0c49f2c
