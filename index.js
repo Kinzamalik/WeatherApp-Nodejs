937a2923bf6aeebecad722407b3ec19302ae860b
Kinzamalik <kiinza.malik@gmail.com>
2021-06-14 21:11:46 +0500

    all

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 // 26101fa61746bcab8ec309c6f0c49f2c
 
 // api.openweathermap.org/data/2.5/weather?q=Pune&appid=26101fa61746bcab8ec309c6f0c49f2c
+
+
+const curDate = document.getElementById('date');
+let weatherIcon = document.getElementById("weathericon");
+
+
+const tempStatus = "Clouds"; 
+
+const getCurrentDay= () =>{
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    let currentTime = new Date();
+    console.log(weekday[currentTime.getDay()])
+};
+
+getCurrentDay()
+
